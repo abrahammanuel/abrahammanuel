a8f7688cc97daa3cd1d0794fe61c154087f3d2f7
Abraham Munguia <abrahamok.munguia@gmail.com>
2019-06-15 19:26:56 -0600

    mi primer pagina con sus fuentes css js y img en github-pages

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+var btnMenu = document.getElementById('btnmenu');
+var menu = document.getElementById('menu');
+
+btnMenu.addEventListener('click', function () {
+    'use strict';
+   menu.classList.toggle('mostrar');});
+
+/* var btnimg = document.getElementById('img-ex');
+btnimg.addEventListener('click', function () {
+  'use stric';
+  btnimg.classList.toggle('expandir');
+}); */
+
+
+/* var todo = document.querySelector("#cajaprinsipal:not(.img-ex)");
+btnimg.addEventListener('click', function(){
+  'use stric';
+  todo.classList.toggle('obscurecer');
+}); */
+
